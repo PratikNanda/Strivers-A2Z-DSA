c5785106a81c5da1e0a04101f88e99cca240e6e4
PratikNanda <pratik.nanda99@gmail.com>
2025-08-27 20:24:46 +0530

    solved count of digits with different approach

--- Step_1/Lec_4/Count_digits_in_a_number.py
+++ Step_1/Lec_4/Count_digits_in_a_number.py
@@ -1,17 +1,26 @@
+# def count_digits(n):
+#     count = 0
+#     convert = str(n)
+#     for _ in convert:
+#         count +=1
+#     return count
+
+# def count_digits(n):
+#     count = 0
+#     while n>0:
+#         count += 1
+#         n = n//10
+#     return count
+
+import math
 def count_digits(n):
-    count = 0
-    convert = str(n)
-    for _ in convert:
-        count +=1
+    count = int(math.log10(n)+1)
     return count
-
-   
-
 
 def main():
     number = int(input("Enter the Number: "))
-    total = count_digits(number)
-    print(total)
+    digits = count_digits(number)
+    print(digits)
 
 
 if __name__ == "__main__":
